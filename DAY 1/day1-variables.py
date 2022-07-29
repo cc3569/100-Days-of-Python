c0685c1aa0c5f7f17acb910ffcb0e11e4f9b4ceb
Cole C <unnecessary4024@gmail.com>
2022-07-29 00:27:50 -0700

    Create day1-variables.py

---
+++ DAY 1/day1-variables.py
@@ -0,0 +1,34 @@
+# 100 Days of Python
+# Cole Catron
+
+input("What is your name? ")
+# The argument will be passed, but the data WILL NOT be stored
+# anywhere and will be deleted from RAM
+
+name = input("What is your name? ")
+print("Hello " + name + "!\n")
+# The input() function is being assigned to the string variable
+# called "name"
+#
+# The print() function prints the contents of the variable "name",
+# which contains the string that the user inputs into the variable
+
+name = "Juan"
+print("Hello " + name + "!")
+# prints "Juan"
+
+name = "Angela"
+print("Hello " + name + "!\n")
+#prints "Hello Angela"
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
+# Input for the name is stored in the variable "name"
+# 
+# The len() function will store the integer into the variable named
+# "length", which will be an integer value
+#
+# The print() will print what is in the "length" variable, which is
+# a numerical value
+
