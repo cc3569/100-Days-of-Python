6e8a17ac94b165d181f7527cb969a669d33f92fb
Cole C <unnecessary4024@gmail.com>
2022-07-29 00:06:13 -0700

    Update day1-input.py

---
+++ DAY 1/day1-input.py
@@ -0,0 +1,18 @@
+# 100 Days of Python
+# Cole Catron
+
+input("What is your name? ")
+# The statement takes in an argument into the console from the user
+# The statement will NOT print anything!
+
+print("Hello " + input("What is your name? ") + "!")
+# The statement will take in an argument into the console from the 
+# user and print it to the console in the format of:
+#       "Hello [user_input]!"
+
+print(len(input("Enter a name: ")))
+# The statement will take in a name as an argument and print the
+# length of the name in terms of the number of characters of the 
+# name given
+
+# The program uses the len() function
