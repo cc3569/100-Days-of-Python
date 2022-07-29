27e5a815979787a221a7733175907b52289b7040
Cole C <unnecessary4024@gmail.com>
2022-07-29 02:44:56 -0700

    Create day2-data_types.py

---
+++ DAY 2/day2-data_types.py
@@ -0,0 +1,47 @@
+# 100 Days of Python
+# Cole Catron
+
+### STRINGS ###
+
+# Strings are a series of characters surrounded by quotation marks.
+# Strings can represent words, phrases, etc.
+
+print("Hello"[0] + "\n")
+
+# The "[0]" refers to the first index of the string "Hello". The
+# console will print the first character of this string. The console
+# will output the following: "H"
+
+print("Hello"[4] + "\n")
+
+# This statement prints "o" to the console terminal.
+
+print("123" + "456\n")
+# The statement will concatenate the two strings rather than adding
+# the two numbers together. The console will out "123456"
+
+### INTEGERS ###
+
+print(123 + 456)
+print("\n") # *Statement for new lines*
+
+# This statement will add the two numbers together and output the
+# result out to the console. The result is an integer value (number)
+#   Result: 579
+
+### FLOATS ###
+
+# Floats are integers that have decimal places.
+
+print(3.14159)
+print("\n") # *Statement for new lines*
+
+### BOOLEANS ###
+
+# Booleans are a data type that can only be true or false
+
+trueVar = True
+falseVar = False
+
+print(trueVar)
+print(falseVar)
