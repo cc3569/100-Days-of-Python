793e0071dd952539cbe07b76217da9a023b783b5
Cole C <unnecessary4024@gmail.com>
2022-07-28 23:48:33 -0700

    Update day1-printing_start.py

--- DAY 1/day1-printing_start.py
+++ DAY 1/day1-printing_start.py
@@ -1,3 +1,21 @@
-print("Hello World!")
-#print("Hello World!) will cause an error
-    #SyntaxError: EOL while scanning string literal
+# 100 Days of Python
+# Cole Catron
+
+print("Hello World!" + "\n")
+# print("Hello World!) will cause an error
+    # SyntaxError: EOL while scanning string literal
+
+print("print('what to print')" + "\n")
+# The single quotation marks allows for the system to print
+# the following statement without throwing a Syntax Error:
+#       print('what to print')
+#
+# If there are double quotations in place, a syntax error will 
+# be thrown to the end user/programmer.
+
+print("Hello World!\n" * 3 + "\n")
+# The following code will output:
+#   Hello World!
+#   Hello World!
+#   Hello World!
+
